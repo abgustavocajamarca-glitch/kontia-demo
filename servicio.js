0b218d8a39e2554d0ed5cb40d14db6f2efdfcd5a
Gustavo Cajamarca <abgustavocajamarca@gmail.com>
2026-09-14 23:44:37 -0500

    Estilo de los desplegables del alta de empresa

--- servicio.js
+++ servicio.js
@@ -18,7 +18,7 @@
  * instala el trabajador nuevo solo, y al activarse borra lo guardado por el
  * anterior (mas abajo). Ya no hay que acordarse de subir un numero a mano.
  */
-const VERSION = '452947e3ae5f';
+const VERSION = '7f8aeed2fd58';
 const CACHE = 'kontia-' + VERSION;
 const PROPIOS = ['./', './index.html', './manifiesto.json',
                  './iconos/icono-192.png', './iconos/icono-512.png'];
