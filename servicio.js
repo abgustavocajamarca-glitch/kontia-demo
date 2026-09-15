5166d092ac7c414acffedd5da2a8f2fc1c2f5f1b
Gustavo Cajamarca <abgustavocajamarca@gmail.com>
2026-09-15 10:46:54 -0500

    Actualiza el sitio

--- servicio.js
+++ servicio.js
@@ -18,7 +18,7 @@
  * instala el trabajador nuevo solo, y al activarse borra lo guardado por el
  * anterior (mas abajo). Ya no hay que acordarse de subir un numero a mano.
  */
-const VERSION = '3b271f278ea4';
+const VERSION = 'ecfd3991bf58';
 const CACHE = 'kontia-' + VERSION;
 const PROPIOS = ['./', './index.html', './manifiesto.json',
                  './iconos/icono-192.png', './iconos/icono-512.png'];
